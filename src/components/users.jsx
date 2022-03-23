a2d8cecb1d1d14dcb04eb4ca1bd93dee6db58e2a
oleg086rus <o.pototskiy@yandex.ru>
2022-03-23 20:01:41 +0500

    some fix after view good decision

--- src/components/users.jsx
+++ src/components/users.jsx
@@ -2,42 +2,13 @@ import React, {useState} from "react";
 import api from '../api';
 
 const Users = () => {
-    const [users, setUsers] = useState(Object.entries(api.users.fetchAll()));
+    const [users, setUsers] = useState(api.users.fetchAll());
     const handleLineDelete = (id) => {
-        setUsers(prevState => prevState.filter(user => user !== id));
+        setUsers(users.filter(user => user._id !== id));
     };
-    const numberOfPeople = () => {
-        const a = `${users.length} человек тусанет с тобой сегодня`;
-        const b = `${users.length} человека тусанут с тобой сегодня`;
-        const c = `Никто с тобой не тусанет`;
-        switch (users.length) {
-            case 12:
-            case 11:
-            case 10:
-            case 9:
-            case 8:
-            case 7:
-            case 6:
-            case 5:
-            case 1:
-                return a;
-            case 2:
-            case 3:
-            case 4:
-                return b;
-            case 0:
-                return c;
-            default:
-                return c;
-        }
-    };
-    const getBageClasses = () => {
-        let classes = 'badge m-2 ';
-        classes += users.length === 0 ? 'bg-danger' : 'bg-primary';
-        return classes;
-    };
+
     const getSpanColor = (el) => {
-        let classes = 'p-1 m-1 badge align-middle bg-';
+        let classes = 'm-1 badge bg-';
         classes += el.color
         return classes;
     };
@@ -45,19 +16,30 @@ const Users = () => {
         return (
             <button type="button"
                     className="btn btn-danger"
-                    onClick={() => handleLineDelete(user)}
+                    onClick={() => handleLineDelete(user._id)}
             >
-                delete
+                Удалить
             </button>
         );
     };
-    const renderBage = () => {
-        return (
-            <h2><span className={getBageClasses()}>{numberOfPeople()}</span></h2>
-        );
-    };
-    const renderTable = () => {
-        return (
+
+    const renderBageText = (number) => {
+        const lastLetter = Number(number.toString().slice(-1));
+        if (number > 4 && number < 15) return "Человек тусанёт с тобой сегодня";
+        if ([2,3,4].indexOf(lastLetter) >= 0) return 'Человека тусанут с тобой сегодня';
+        if (lastLetter === 1) return "Человек тусанёт с тобой сегодня";
+        return "Человек тусанёт с тобой сегодня"
+    }
+
+    return (
+        <>
+            <h2>
+                <span className={'badge bg-' + (!users.length ? 'danger' : 'primary')}>
+                    {
+                        users.length ? `${users.length} ${renderBageText(users.length)}` : 'Никто с тобой не тусанет'
+                    }
+                </span>
+            </h2>
             <table className="table">
                 <thead>
                 <tr>
@@ -70,14 +52,14 @@ const Users = () => {
                 </tr>
                 </thead>
                 <tbody>
-                {users.map((user, id) => (
-                    <tr key={id}>
-                        <td className='align-middle'>{user[1].name}</td>
-                        <td className='align-middle'>{user[1].qualities.map((el, id) => (
-                            <span key={id} className={getSpanColor(el)}>{el.name}</span>))}</td>
-                        <td className='align-middle'>{user[1].profession.name}</td>
-                        <td className='align-middle'>{user[1].completedMeetings}</td>
-                        <td className='align-middle'>{user[1].rate}/5</td>
+                {users.map((user) => (
+                    <tr key={user._id}>
+                        <td className='align-middle'>{user.name}</td>
+                        <td className='align-middle'>{user.qualities.map((el) => (
+                            <span key={el._id} className={getSpanColor(el)}>{el.name}</span>))}</td>
+                        <td className='align-middle'>{user.profession.name}</td>
+                        <td className='align-middle'>{user.completedMeetings}</td>
+                        <td className='align-middle'>{user.rate}/5</td>
                         <td className='align-middle'>
                             {deleteButton(user)}
                         </td>
@@ -85,21 +67,6 @@ const Users = () => {
                 ))}
                 </tbody>
             </table>
-        );
-    };
-
-    if (users.length !== 0) {
-        return (
-            <>
-                {renderBage()}
-                {renderTable()}
-            </>
-        );
-    }
-
-    return (
-        <>
-            {renderBage()}
         </>
     );
 };
