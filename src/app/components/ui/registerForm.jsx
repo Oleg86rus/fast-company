0d238daa697d6824ae0f17655c09520fcdc9d706
oleg086rus <o.pototskiy@yandex.ru>
2022-05-06 01:30:27 +0500

    add select profeccion in registration form

--- src/app/components/ui/registerForm.jsx
+++ src/app/components/ui/registerForm.jsx
@@ -1,12 +1,22 @@
 import React, { useEffect, useState } from 'react';
 import { validator } from '../../utils/validator';
 import TextField from '../common/form/textField';
+import API from '../../api';
+import SelectField from '../common/form/selectField';
+
 const RegisterForm = () => {
-  const [data, setData] = useState({email: '', password: ''});
+  const [data, setData] = useState({ email: '', password: '', profession: '' });
   const [errors, setErrors] = useState({});
-  const handleChange = ({target}) => {
-    setData((prevState)=>({...prevState, [target.name]: target.value}));
+  const [professions, setProfessions] = useState([]);
+  const handleChange = ({ target }) => {
+    setData((prevState) => ({ ...prevState, [target.name]: target.value }));
   };
+  useEffect(() => {
+    // eslint-disable-next-line no-shadow
+    API.professions.fetchAll().then((data) => setProfessions(data));
+  },
+  []);
+
   const validatorConfig = {
     email: {
       isRequired: {
@@ -30,19 +40,26 @@ const RegisterForm = () => {
         message: 'Пароль должен состоять минимум из 8 символов',
         value: 8
       }
+    },
+    profession: {
+      isRequired: {
+        message: 'Обязательно выберете вашу профессию'
+      }
     }
   };
   
   const validate = () => {
     // eslint-disable-next-line no-shadow
-    const errors = validator(data, validatorConfig);
+    const errors = validator(data,
+      validatorConfig);
     setErrors(errors);
     return Object.keys(errors).length === 0;
   };
   
-  useEffect(()=> {
+  useEffect(() => {
     validate();
-  }, [data]);
+  },
+  [data]);
   const isValid = Object.keys(errors).length === 0;
   
   const handleSubmit = (e) => {
@@ -56,21 +73,35 @@ const RegisterForm = () => {
   return (
     <form onSubmit={handleSubmit}>
       <TextField
-        label='Электронная почта'
-        name='email'
+        label="Электронная почта"
+        name="email"
         value={data.email}
         onChange={handleChange}
         error={errors.email}
       />
       <TextField
-        label='Пароль'
-        type='password'
-        name='password'
+        label="Пароль"
+        type="password"
+        name="password"
         value={data.password}
         onChange={handleChange}
         error={errors.password}
       />
-      <button className='btn btn-primary w-100 mx-auto' type='submit' disabled={!isValid}>Submit</button>
+      <SelectField
+        label='Выбери свою профессию'
+        options={professions}
+        defaultOption='Choose...'
+        onChange={handleChange}
+        value={data.profession}
+        error={errors.profession}
+      />
+      <button
+        className="btn btn-primary w-100 mx-auto"
+        type="submit"
+        disabled={!isValid}
+      >
+        Submit
+      </button>
     </form>
   );
 };
