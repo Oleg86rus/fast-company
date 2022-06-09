0a8b5aadb65e30d4419e0ad316a0861cd0ae0a14
oleg086rus <o.pototskiy@yandex.ru>
2022-06-09 23:43:11 +0500

    create localStorageService file

--- src/app/hooks/useAuth.jsx
+++ src/app/hooks/useAuth.jsx
@@ -3,24 +3,18 @@ import PropTypes from 'prop-types';
 import axios from 'axios';
 import userService from '../service/user.service';
 import { toast } from 'react-toastify';
+import { setTokens } from '../service/localStorage.service';
 const httpAuth = axios.create();
 const AuthContext = React.createContext();
 
 export const useAuth = () => {
   return useContext(AuthContext);
 };
-const TOKEN_KEY = 'jwt-token';
-const REFRESH_KEY = 'jwt-refresh-token';
-const EXPIRES_KEY = 'jwt-expires';
+
 const AuthProvider = ({children}) => {
   const [currentUser, setUser] = useState({});
   const [error, setError] = useState(null);
-  function setTokens({refreshToken, idToken, expiresIn=3600}) {
-    const expireDate = new Date().getTime()+expiresIn*1000;
-    localStorage.setItem(TOKEN_KEY, idToken);
-    localStorage.setItem(REFRESH_KEY, refreshToken);
-    localStorage.setItem(EXPIRES_KEY, expireDate);
-  }
+
   function errorCatcher(error) {
     const {message} = error.response.data;
     setError(message);
