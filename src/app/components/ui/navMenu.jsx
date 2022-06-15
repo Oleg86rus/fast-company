10878164bd3b7401c3320da0685ce1f606fe0b56
oleg086rus <o.pototskiy@yandex.ru>
2022-06-16 00:17:17 +0500

    some fix in nav, display nav by authorization

--- src/app/components/ui/navMenu.jsx
+++ src/app/components/ui/navMenu.jsx
@@ -1,33 +1,26 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import { useAuth } from '../../hooks/useAuth';
 
 const NavMenu = () => {
-  const menus = [
-    {
-      id: 0,
-      name: 'Main',
-      link: '/'
-    },
-    {
-      id: 1,
-      name: 'Login',
-      link: '/login'
-    },
-    {
-      id: 2,
-      name: 'Users',
-      link: '/users'
-    }
-  ];
-
+  const {currentUser} = useAuth();
   return (
-    <ul className="nav nav-tabs">
-      {menus.map(el=>(
-        <li key={el.id} className="nav-item">
-          <NavLink exact className='nav-link' to={el.link}>{el.name}</NavLink>
-        </li>
-      ))}
-    </ul>
+    <nav className='navbar bg-light mb-3'>
+      <div className="container-fluid">
+        <ul className="nav">
+          <li className="nav-item">
+            <Link exact className='nav-link' to='/'>Main</Link>
+          </li>
+          {currentUser && (<li className="nav-item">
+            <Link exact className='nav-link' to='/users'>Users</Link>
+          </li>)}
+        </ul>
+        <div className="d-flex">
+          {currentUser ? <p>User</p> : <Link exact className='nav-link' to='/login'>Login</Link>}
+          
+        </div>
+      </div>
+    </nav>
   );
 };
 export default NavMenu;
