dbb8aa728da541be396e071bcd047033eba30f2a
oleg086rus <o.pototskiy@yandex.ru>
2022-06-28 18:45:12 +0500

    create userSlice

---
+++ src/app/store/users.js
@@ -0,0 +1,40 @@
+import { createSlice } from '@reduxjs/toolkit';
+import userService from '../service/user.service';
+
+const usersSlice = createSlice({
+  name: 'users',
+  initialState: {
+    entities: null,
+    isLoading: true,
+    error: null,
+    lastFetch: null
+  },
+  reducers: {
+    usersRequested: state => {
+      state.isLoading = true;
+    },
+    usersReceved: (state, action) => {
+      state.entities = action.payload;
+      state.isLoading = false;
+    },
+    usersRequestFiled: (state, action) => {
+      state.error = action.payload;
+      state.isLoading = false;
+    }
+  }
+});
+
+const {reducer: usersReducer, actions} = usersSlice;
+const {usersRequested, usersReceved, usersRequestFiled} = actions;
+
+export const loadUsersList = () => async (dispatch, getState) => {
+  dispatch(usersRequested());
+  try {
+    const { content } = await userService.get();
+    dispatch(usersReceved(content));
+  } catch (error) {
+    dispatch(usersRequestFiled(error.message));
+  }
+};
+
+export default usersReducer;
