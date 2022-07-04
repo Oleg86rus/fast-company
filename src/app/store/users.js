abbadb6ad681bb646e129d990cf6337ced228f84
oleg086rus <o.pototskiy@yandex.ru>
2022-07-04 20:23:56 +0500

    authRequested errors

--- src/app/store/users.js
+++ src/app/store/users.js
@@ -4,6 +4,7 @@ import authService from '../service/auth.service';
 import localStorageService from '../service/localStorage.service';
 import { getRandomInt } from '../utils/getRandomInt';
 import history from '../utils/history';
+import { generateAuthError } from '../utils/generateAuthError';
 
 const initialState = localStorageService.getAccessToken() ? {
   entities: null,
@@ -33,7 +34,7 @@ const usersSlice = createSlice({
       state.dataLoaded = true;
       state.isLoading = false;
     },
-    usersRequestFiled: (state, action) => {
+    usersRequestFailed: (state, action) => {
       state.error = action.payload;
       state.isLoading = false;
     },
@@ -58,7 +59,11 @@ const usersSlice = createSlice({
     },
     userUpdate: (state, action) => {
       state.entities[state.entities.findIndex(u=>u._id===action.payload._id)] = action.payload;
+    },
+    authRequested: (state) => {
+      state.error = null;
     }
+    
   }
 });
 
@@ -66,7 +71,7 @@ const {reducer: usersReducer, actions} = usersSlice;
 const {
   usersRequested,
   usersReceved,
-  usersRequestFiled,
+  usersRequestFailed,
   authRequestSuccess,
   authRequestFailed,
   userCreated,
@@ -89,7 +94,14 @@ export const login = ({ payload, redirect }) => async (dispatch) => {
     localStorageService.setTokens(data);
     history.push(redirect);
   } catch (error) {
-    dispatch(authRequestFailed(error.message));
+    const {code, message} = error.response.data.error;
+    if (code === 400) {
+      const errorMessage = generateAuthError(message);
+      dispatch(authRequestFailed(errorMessage));
+    } else {
+      dispatch(authRequestFailed(error.message));
+    }
+    
   }
 };
 
@@ -141,7 +153,7 @@ export const loadUsersList = () => async (dispatch, getState) => {
     const { content } = await userService.get();
     dispatch(usersReceved(content));
   } catch (error) {
-    dispatch(usersRequestFiled(error.message));
+    dispatch(usersRequestFailed(error.message));
   }
 };
 
@@ -168,4 +180,5 @@ export const getIsLoggedIn = () => state => state.users.isLoggedIn;
 export const getDataStatus = () => state => state.users.dataLoaded;
 export const getUsersLoadingStatus = () => state => state.users.isLoading;
 export const getCurrentUserId = () => state => state.users.auth.userId;
+export const getAuthErrors = () => state => state.users.error;
 export default usersReducer;
